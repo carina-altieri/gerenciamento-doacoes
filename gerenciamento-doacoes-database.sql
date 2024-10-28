a30f2e7dca8075efb6cfdb8368e7224cc1f0d00e
Carina Altieri <carinaaltieri.08@gmail.com>
2024-10-27 21:34:37 -0300

    correção de tabelas

--- gerenciamento-doacoes-database.sql
+++ gerenciamento-doacoes-database.sql
@@ -2,39 +2,37 @@ CREATE DATABASE gerenciamentoDoacoes;
 USE gerenciamentoDoacoes;
 
 CREATE TABLE doador (
-id_doador INT AUTO_INCREMENT PRIMARY KEY,
-RG VARCHAR(9) UNIQUE NOT NULL,
+RG VARCHAR(9) UNIQUE NOT NULL PRIMARY KEY,
 nome VARCHAR(255) NOT NULL,
 telefone VARCHAR (18) NOT NULL,
 email VARCHAR (50) NOT NULL
 );
 
-
-
 CREATE TABLE doação ( 
 id_doacao INT AUTO_INCREMENT PRIMARY KEY,
-id_instituicao INT,
-id_doador INT,
-id_beneficiario INT,
-descricao VARCHAR (100) NOT NULL,
-categoria VARCHAR (50) NOT NULL
+cnpj VARCHAR(19) NOT NULL,
+RG VARCHAR(9) UNIQUE NOT NULL,
+cnpj_beneficiario VARCHAR(19) NOT NULL,
+descricao VARCHAR(100) NOT NULL,
+categoria VARCHAR(50) NOT NULL,
+FOREIGN KEY (CNPJ) REFERENCES instituição(CNPJ),
+FOREIGN KEY (RG) REFERENCES doador(RG)
 );
 
 CREATE TABLE instituição (
-id_instituicao INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR (255) NOT NULL,
-cnpj VARCHAR(18) NOT NULL
+cnpj VARCHAR(18) NOT NULL PRIMARY KEY,
+nome VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE rastreio (
 id_rastreio INT AUTO_INCREMENT PRIMARY KEY,
 id_doacao INT,
-status VARCHAR (50) NOT NULL,
-date_status DATE NOT NULL
+status VARCHAR(50) NOT NULL,
+FOREIGN KEY (id_doacao) REFERENCES doação(id_doacao)
 );
 
 CREATE TABLE instituição_beneficiada (
-id_beneficiario INT AUTO_INCREMENT PRIMARY KEY,
+CNPJ_beneficiario VARCHAR(19) NOT NULL PRIMARY KEY,
 nome VARCHAR (255) NOT NULL,
 endereco VARCHAR (200) NOT NULL,
 email VARCHAR (50) NOT NULL,
